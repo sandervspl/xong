14c23a784a565109c69416ee8368ef97b50f27f9
sandervspl <sandervispoel@hotmail.com>
2022-01-29 19:55:41 +0100

    Add keypress listeners

--- apps/web/pages/game/[gameId].tsx
+++ apps/web/pages/game/[gameId].tsx
@@ -3,9 +3,54 @@ import type { GetServerSideProps } from 'next';
 
 import type { GetGameByIdQuery } from 'faunadb/generated';
 import { sdk } from 'lib/fauna';
+import useSocketIO from 'hooks/useSocketIO';
+import useLocalStorage from 'hooks/userLocalStorage';
+import { useEventListener } from 'hooks/useEventListener';
 
 
 const GameLobby: React.VFC<Props> = (props) => {
+  const socket = useSocketIO();
+  const { getItem } = useLocalStorage();
+  const playerIds = props.game!.players.data.map((player) => player!._id);
+  const [keysPressed, setKeysPressed] = React.useState<Record<string, boolean>>({});
+  const userIsPlayer = getItem('user_id') && playerIds.includes(getItem('user_id')!);
+
+  React.useEffect(() => {
+    socket?.emit('user-joined-game', getItem('user_id'));
+  }, [socket]);
+
+  useEventListener('keydown', (e) => {
+    if (!userIsPlayer || keysPressed[e.key]) {
+      return;
+    }
+
+    setKeysPressed((draft) => {
+      draft[e.key] = true;
+      return draft;
+    });
+
+    socket?.emit('player-key-down', {
+      userId: getItem('user_id'),
+      key: e.key,
+    });
+  });
+
+  useEventListener('keyup', (e) => {
+    if (!userIsPlayer) {
+      return;
+    }
+
+    setKeysPressed((draft) => {
+      draft[e.key] = false;
+      return draft;
+    });
+
+    socket?.emit('player-key-up', {
+      userId: getItem('user_id'),
+      key: e.key,
+    });
+  });
+
   return (
     <div className="text-primary-500">
       <h1 className="text-9xl">Game Lobby</h1>
