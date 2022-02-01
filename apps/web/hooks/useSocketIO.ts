2d106f7ef31cb4f23f6036f6de11a7255a6732e8
sandervspl <sandervispoel@hotmail.com>
2022-02-01 18:01:45 +0100

    Fix socket io connect spam

--- apps/web/hooks/useSocketIO.ts
+++ apps/web/hooks/useSocketIO.ts
@@ -3,18 +3,22 @@ import type { Socket } from 'socket.io-client';
 import { connect } from 'socket.io-client';
 
 
+// Get socket.io connection on client started
+const socket = connect(process.env.NEXT_PUBLIC_WEBSOCKET_URL!);
+let hasClient = false;
+
 function useSocketIO() {
   const [ioClient, setIoClient] = React.useState<Socket | null>(null);
 
   React.useEffect(() => {
-    // Get socket.io connection on client started
-    const socket = connect(process.env.NEXT_PUBLIC_WEBSOCKET_URL!);
+    if (!hasClient) {
+      socket.on('connect', () => {
+        console.info('connected to socket server!');
+      });
 
-    socket.on('connect', () => {
-      console.info('connected to socket server!');
-    });
-
-    setIoClient(socket);
+      setIoClient(socket);
+      hasClient = true;
+    }
   }, []);
 
   return ioClient;
