bad11d0a63b200a670288ccf70071ce3ce6e8fc0
sandervspl <sandervispoel@hotmail.com>
2022-02-04 17:05:29 +0100

    Add socket constants

--- packages/constants/sockets.ts
+++ packages/constants/sockets.ts
@@ -1,1 +1,9 @@
+export const USER_JOINED_GAME = 'user-joined-game';
+export const USER_LEFT_GAME = 'user-left-game';
+export const PLAYER_CONNECT_UPDATE = 'player-connect-update';
+export const GAME_PLAYSTATE_UPDATE = 'game-playstate-update';
+export const PLAYER_KEY_DOWN = 'player-key-down';
+export const PLAYER_KEY_UP = 'player-key-up';
+export const PLAYER_SELECT_CELL = 'player-select-cell';
 export const PLAYER_HIT_CELL = 'player-hit-cell';
+export const BALL_HIT_OBJECT = 'ball-hit-object';
